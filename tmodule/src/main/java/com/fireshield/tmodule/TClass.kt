f9f9de47d36f71ed9f817e4315396362e92f1a52
Daniel Sanchez <daniel.ceinos@gmail.com>
2018-07-10 00:23:44 +0200

    test

--- tmodule/src/main/java/com/fireshield/tmodule/TClass.kt
+++ tmodule/src/main/java/com/fireshield/tmodule/TClass.kt
@@ -1,11 +1,13 @@
 package com.fireshield.tmodule
 
+import io.reactivex.Single
+
 /**
  * Created by Daniel S on 10/07/2018.
  */
 class TClass{
 
   fun obObservable() : Single<String> {
-
+      return Single.just("holi")
   }
 }
